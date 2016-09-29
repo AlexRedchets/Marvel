7e4bbb2ac723a019c39497648effa11c213cc71f
AlexRedchets <alexey.redchets@gmail.com>
2016-09-29 14:10:47 -0400

    Marvel

--- app/src/main/java/com/azvk/marvel/marvel/MarvelPresenter.java
+++ app/src/main/java/com/azvk/marvel/marvel/MarvelPresenter.java
@@ -21,8 +21,8 @@ public class MarvelPresenter implements MarvelInterface.Presenter{
     private Realm realm;
 
     private static final String TAG = MarvelPresenter.class.getSimpleName();
-    private static final String API_KEY = "01fdd5eea27a50607e18d9dc5864933e";
-    private static final String PRIVATE_KEY = "02ef461fb953d3750f0e60e00bf7d2f8b731462d";
+    private static final String first = "01fdd5eea27a50607e18d9dc5864933e";
+    private static final String second = "02ef461fb953d3750f0e60e00bf7d2f8b731462d";
 
     @Inject
     public MarvelPresenter(Retrofit retrofit, MarvelInterface.View view, Realm realm) {
@@ -35,8 +35,8 @@ public class MarvelPresenter implements MarvelInterface.Presenter{
     public void fetchData() {
         Integer limit = 15;
         long ts = System.currentTimeMillis();
-        String hash = DigestUtils.md5Hex(ts + PRIVATE_KEY + API_KEY);
-        retrofit.create(MarvelClient.class).getRepos(limit, ts, API_KEY, hash)
+        String hash = DigestUtils.md5Hex(ts + second + first);
+        retrofit.create(MarvelClient.class).getRepos(limit, ts, first, hash)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .unsubscribeOn(Schedulers.io())
